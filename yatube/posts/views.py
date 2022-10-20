77109243361316f55913783e522caedf8433a7c6
Мажид Азиев <majidaziev007@yandex.ru>
2022-10-20 22:04:19 +0300

    BETA Версия

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import render,  get_object_or_404
+
+from .models import Post, Group
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    template = 'posts/index.html'
+
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Сообщество номер {slug}') 
+
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
